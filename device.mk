010036a282b0aaff821322be3e503bd40eec4900
WilliamSado <williamsado1004@gmail.com>
2025-07-27 22:25:18 +0800

    ishtar: Import device specific overlay
    Change-Id: Ic7739f85df881874a47636e1a81f308b2d1cb704

--- device.mk
+++ device.mk
@@ -6,6 +6,16 @@
 # Inherit from sm8550-common
 $(call inherit-product, device/xiaomi/sm8550-common/common.mk)
 
+# Overlay
+PRODUCT_PACKAGES += \
+    FrameworkResOverlayIshtar \
+    SettingsOverlayIshtar \
+    SettingsProviderResIshtar \
+    SystemUIOverlayIshtar
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/overlay/config-odm.xml:$(TARGET_COPY_OUT_ODM)/overlay/config/config.xml
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
