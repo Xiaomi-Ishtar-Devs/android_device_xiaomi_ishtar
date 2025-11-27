e1e0190557820e1dd7371dea24dcd7a9063f1024
lolipuru <banstellazz@gmail.com>
2025-11-27 07:16:33 +0000

    ishtar: Commonized xiaomi.sensors.v2
    Change-Id: I6d47307b52ebdc6270163a61db9a4bb7baed7818

--- device.mk
+++ device.mk
@@ -11,10 +11,6 @@ $(call inherit-product, device/xiaomi/sm8550-common/common.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/mixer_paths_kalama_mtp.xml \
     $(LOCAL_PATH)/audio/resourcemanager_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/resourcemanager_kalama_mtp.xml
-
-# Init
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init/init.ishtar.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.ishtar.rc \
     
 # IFAAService
 PRODUCT_PACKAGES += \
@@ -33,13 +29,6 @@ PRODUCT_PACKAGES += \
 $(call soong_config_set,lineage_powershare,powershare_path,/sys/class/qcom-battery/reverse_chg_mode)
 PRODUCT_PACKAGES += \
     vendor.lineage.powershare-service.default
-
-# Sensors
-PRODUCT_PACKAGES += \
-    sensors.xiaomi.v2
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/hals.conf:$(TARGET_COPY_OUT_ODM)/etc/sensors/hals.conf
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
