7ac8081b009c7e15bcde63e4f2cc6f9bc29e68c2
WilliamSado <williamsado1004@gmail.com>
2026-07-19 04:10:58 +0800

    ishtar: Configure AviumUI build
    Change-Id: Ibc029f123931dd94a18195ce83ca0393b6b2506e

--- lineage_ishtar.mk
+++ lineage_ishtar.mk
@@ -5,7 +5,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from ishtar device
@@ -19,6 +19,14 @@ PRODUCT_DEVICE := ishtar
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 2304FPN6DC
+
+# AviumUI Configs
+AVIUM_MAINTAINER := WilliamSado
+AVIUM_SETTINGS_SOC_MODEL_NAME := Qualcomm Snapdragon 8 Gen 2
+AVIUM_SETTINGS_DEVICE_CODENAME := Xiaomi 13 Ultra
+AVIUM_IS_OFFICIAL := false
+WITH_GMS := true
+AVIUM_FORCE_SET_FAKE_PROP := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
